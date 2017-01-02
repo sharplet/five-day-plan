62d10a32850e08f74314e3073c154ab3a94b5d36
Adam Sharp <adam@sharplet.me>
2017-01-02 12:36:13 -0500

    Set up testing with Quick + Nimble

---
+++ Tests/FiveDayPlanTests/FiveDayPlanSpec.swift
@@ -0,0 +1,11 @@
+@testable import FiveDayPlan
+import Quick
+import Nimble
+
+final class FiveDayPlanSpec: QuickSpec {
+  override func spec() {
+    it("works") {
+      expect(1 + 1) == 2
+    }
+  }
+}
--- Tests/FiveDayPlanTests/FiveDayPlanTests.swift
+++
@@ -1,8 +0,0 @@
-@testable import FiveDayPlan
-import XCTest
-
-final class FiveDayPlanTests: XCTestCase {
-  func testExample() {
-    XCTAssertEqual(1 + 1, 2)
-  }
-}
